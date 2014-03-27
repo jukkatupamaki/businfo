66465d84eae2d864fb7a4c4fd986bb97cbbcb1a5
Jukka Tupamaki <jukka.tupamaki@futurice.com>
2014-03-27 15:38:50 +0200

    JSON parsing implemented

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 var request = require('request');
 
-request(
+request.get(
 	"http://data.itsfactory.fi/siriaccess/vm/rest", 
 	function handleJSONResponse(error, response, body) {
-		console.log(error, response, body);
+		if(parseInt(response.statusCode, 10) !== 200 || error) {
+			console.log(error);
+		} else {
+			// Parse the JSON string in 'body' to a JavaScript object
+			var jsonData = JSON.parse(body);
+
+			// The JSON response is very deep. 
+			var vehicleActivity = jsonData.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity;
+
+			// Re-format the objects to be a bit more useful
+			var activeBusLines = {};
+
+			// The JSON is a bit badly formed for our use so lets re-format it.
+			vehicleActivity.forEach(function(data) {
+				// Take the most interesting object only to shorten references
+				data = data.MonitoredVehicleJourney;
+
+				// Lines can have multiple buses, or vehicles, at the same time.
+				// Create an array for the bus line if it does not exist yet
+				if(!activeBusLines[data.LineRef.value]) {
+					activeBusLines[data.LineRef.value] = [];
+				}
+
+				var vehicleData = {
+					lineRef: data.LineRef.value,
+					from: data.OriginName.value,
+					to: data.DestinationName.value,
+					location: {
+						latitude: data.VehicleLocation.Longitude,
+						longitude: data.VehicleLocation.Latitude,
+						bearing: data.Bearing
+					},
+					vehicleRef: data.VehicleRef.value
+				};
+
+				// Push the vehicle data to the correct array
+				activeBusLines[data.LineRef.value].push(vehicleData);
+			});
+
+			console.log(activeBusLines);
+		}
 	}
 );
